de9760b46904981fcdec819b0baa99f84f976ff6
Iván Legrán <68439095+ivanlegranbizarro@users.noreply.github.com>
2022-12-27 07:34:47 +0100

    fixing typos

--- blog/urls.py
+++ blog/urls.py
@@ -3,12 +3,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # Categorie's urls
     path('category-list', views.CategoryList.as_view(), name='category-list'),
     path('category/<slug:slug>', views.CategoryRetrieveView.as_view(),
          name='category-detail'),
+    # Author's urls
     path('author-list', views.AuthorList.as_view(), name='author-list'),
     path('admin-authors/<int:pk>',
          views.AdminAuthorsList.as_view(), name='admin-authors'),
+    # Post's urls
     path('post-list', views.PostList.as_view(), name='post-list'),
     path('post/<int:pk>', views.PostRetrieveView.as_view(), name='post-detail'),
     path('admin-post/<int:pk>',
--- blogDRF/settings.py
+++ blogDRF/settings.py
@@ -139,12 +139,12 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-# Authentication settings for REST_FRAMEWORK
+# REST_FRAMEWORK Settings
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 2
+    'PAGE_SIZE': 10
 }
